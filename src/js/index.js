75db6578084db4c2eb8b2fdde58293fe47d8a6ed
Deepak Joy Jose <deejayjay@zoho.com>
2022-07-19 14:06:21 -0600

    Adds styles for Desktop layout. Adds animations for interactivity.

--- src/js/index.js
+++ src/js/index.js
@@ -11,6 +11,11 @@ async function loadNewAdvice() {
   const API_URL = "https://api.adviceslip.com/advice";
   const adviceNumber = document.getElementById("adviceNumber");
   const adviceContent = document.getElementById("adviceContent");
+  const divider = document.getElementById("divider");
+
+  adviceNumber.classList.remove("advice-loaded");
+  adviceContent.classList.remove("advice-loaded");
+  divider.classList.remove("advice-loaded");
 
   // Fetch data from the Advice Slip JSON API
   const response = await fetch(API_URL);
@@ -18,11 +23,19 @@ async function loadNewAdvice() {
 
   console.log(data);
 
+  if (!data) {
+    return;
+  }
+
+  adviceNumber.classList.add("advice-loaded");
+  adviceContent.classList.add("advice-loaded");
+  divider.classList.add("advice-loaded");
+
   // Extract id and advice from the API response
   const { id, advice } = data.slip;
 
   console.log(id, advice);
 
   adviceNumber.textContent = id;
-  adviceContent.textContent = `"${advice}"`;
+  adviceContent.textContent = `“${advice}”`;
 }
